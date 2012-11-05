53d4b1036a0b19ba53ee9c74ceb742b4b77a2d84
Pedro Hernández <pedroh.r99@gmail.com>
2012-11-05 11:17:58 +0000

    Renombro carpeta TocadiscosViewController a TocadiscosRetroViewController

---
+++ tocadiscos/tocadiscos/TocadiscosRetroViewController.m
@@ -0,0 +1,38 @@
+//
+//  TocadiscosRetroViewController.m
+//  tocadiscos
+//
+//  Created by Lion User on 05/11/12.
+//  Copyright (c) 2012 LexyPlanet. All rights reserved.
+//
+
+#import "TocadiscosRetroViewController.h"
+
+@interface TocadiscosRetroViewController ()
+
+@end
+
+@implementation TocadiscosRetroViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
